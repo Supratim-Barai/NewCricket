6ec906fe3a31a68b368dbea51eae22f36a468b15
Goutam Das <goutamdas676@gmail.com>
2023-02-07 01:40:30 +0530

    base url changes

--- src/constants.ts
+++ src/constants.ts
@@ -1,8 +1,8 @@
 export const USER_DETAILS = 'USER_DETAILS';
 
 // base url for api call
-export const BASE_URL = 'http://52.66.245.248:3000/api';
-export const SOCKET_URL = "http://52.66.245.248:3001";
+export const BASE_URL = 'http://31.220.59.111:3000/api';
+export const SOCKET_URL = "http://31.220.59.111:3001";
 
 // x-access-token
 export const X_ACCESS_TOKEN = '$2a$10$g8VPdJeuxZg2uKRsvtBSSOydJdWlKWapBh8Y91juqx8trXp3RkR5C';
